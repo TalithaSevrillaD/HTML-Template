617ded9a19d35a0e87cac30ecaa26a59d7db9e37
TalithaSevrillaD <talitha_sevrilla_27rpl@student.smktelkom-mlg.sch.id>
2020-10-18 13:49:26 +0700

    1st commit

---
+++ views/blog/index.blade.php
@@ -0,0 +1,43 @@
+<div id="blog">
+    <div class="sidebar">
+        <div class="section topics">
+            <h2 class="section-title">Recent Blog</h2>
+            <ul>
+                <li><a href="#">The Most Stylish Shoes Men Can Wear To Work</a></li>
+                <li><a href="#">Shoes Is A Statement Of All</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="row mb-2">
+        <div class="col-md-6">
+            <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="col p-4 d-flex flex-column position-static">
+                    <h3 class="mb-0">The Most Stylish Shoes Men Can Wear To Work</h3>
+                    <div class="dropdown-divider"></div> 
+                    <p class="card-text mb-auto"> Many people would agree that you can tell a lot about a man by the shoes he wears. 
+                        Choosing the right pair of shoes enhances your attire</p>
+                    <a href="blog-detail-1.html" class="stretched-link">Continue reading</a>
+                </div>
+                <div class="col-auto d-none d-lg-block">
+                    <img src="images/artikel.jpg" style="height: 250px; width:150px;">
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6"  >
+            <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="col p-4 d-flex flex-column position-static">
+                    <h3 class="mb-0">Shoes Is A Statement Of All</h3>
+                    <div class="dropdown-divider"></div> 
+                    <p class="card-text mb-auto"> In today's fashion world shoes have to be more versatile than ever. 
+                        Unless you stay home all the time, it's likely that you have to wear 
+                        shoes of some sort every day. </p>
+                    <a href="blog-detail-2.html" class="stretched-link">Continue reading</a>
+                </div>
+                <div class="col-auto d-none d-lg-block">
+                    <img src="images/artikel1.jpg" style="height: 250px; width:150px;">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
